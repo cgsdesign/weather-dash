ebed22e4df695f09d476a5544329dde52552ccd2
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-10-10 13:55:18 -0500

    Initial HTML and CSS uploaded

---
+++ weather-dash.js
@@ -0,0 +1,10 @@
+//Plan:
+//make HTML page outline
+//set up on click response for city
+//set up cards (bootstrap)
+//set up weather API
+    //to pulls:
+    // city name, date & 
+    // Current: wather condition (icon/image) temp, humidity, wind speed, UV(color coded)
+    //5 day: weather, wind spd, humidity, temp
+// city added to search history   
